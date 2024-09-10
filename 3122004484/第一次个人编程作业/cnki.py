fb45897defc2bad67ec17f73479d51037868c1dc
LC <Lc258932589@outlook.com>
2024-09-10 17:37:55 +0800

    完成读取文件功能

--- 3122004484/第一次个人编程作业/cnki.py
+++ 3122004484/第一次个人编程作业/cnki.py
@@ -1,1 +1,46 @@
-print("test")
+"""
+1. 读取文件内容
+2. 预处理文本
+3. 计算词频向量
+4. 计算余弦相似度
+5. 输出结果
+"""
+import sys
+
+# 读取文件内容
+def read_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            text = f.read()
+    except FileNotFoundError:
+        print(f"文件{file_path}未找到，请检查文件路径是否正确。")
+    except PermissionError:
+        print(f"没有权限读取文件{file_path}，请检查该文件的权限设置。")
+    except UnicodeError:
+        print("文件编码错误，请尝试使用其他编码格式读取。")
+    except IOError:
+        print(f"文件{file_path}读取时发生I/O错误，请检查硬盘或文件系统")
+    except Exception as e:
+        print(f"读取文件{file_path}发生未知错误：{e}")
+    return text
+
+
+
+def main():
+    # 从命令行获取参数
+    if len(sys.argv) != 4:
+        print("用法: python cnki.py <原文文件路径> <抄袭版文件路径> <输出结果文件路径>")
+        return
+
+    original_file = sys.argv[1]
+    plagiarized_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    # 读取文件内容
+    original_text = read_file(original_file)
+    plagiarized_text = read_file(plagiarized_file)
+    output_text = read_file(output_file)
+
+
+if __name__ == "__main__":
+    main()
